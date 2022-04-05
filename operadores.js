1729e7804a47356077c98f2d4a6cfa261f681693
jorgecruzprof@gmail.com <jorgecruzprof@gmail.com>
2022-04-05 02:56:41 -0500

    5 abril, 2:56 am

--- operadores.js
+++ operadores.js
@@ -1,7 +1,47 @@
 // Operador de igualdad  ==
-console.log(6 == 6);
-console.log(6 == 5);
-console.log("Hola" == "hola");
-console.log([1,2,3] == [1,2,3]);
+console.log(6 == 6); //true
+console.log(6 == 5); //false
+console.log("Hola" == "hola"); //false
+console.log([1,2,3] == [1,2,3]); //false
+console.log(9 == "9"); //true
 
 //Operador de igualdad estricta
+console.log(9 === "9"); //false
+console.log(9 === 9); //true
+
+//Operador de desigualdad
+console.log(9 != 6); //true
+console.log(9 != '9'); //false
+
+//Operador de desigualdad estricta
+console.log(1 !== "1"); //true
+
+//Operador mayor que
+console.log(5 > 2); //true
+console.log(3 > 8); //false
+console.log(1 > 1); //false
+console.log("mundo" > "hola"); //true
+console.log("ABC" > "ACB"); //false
+
+//Operador mayor o igual que
+console.log(5 >= 2); //true
+console.log(3 >= 8); //false
+console.log(1 >= 1); //true
+console.log("mundo" >= "hola"); //true
+console.log("ABC" >= "ACB"); //false
+
+//Operador menor que
+console.log(5 < 2); //false
+console.log(3 < 8); //true
+console.log(1 < 1); //false
+console.log("mundo" < "hola"); //false
+console.log("ABC" < "ACB"); //true
+
+//Operador menor o igual que
+console.log(5 <= 2); //false
+console.log(3 <= 8); //true
+console.log(1 <= 1); //true
+console.log("mundo" <= "hola"); //false
+console.log("ABC" <= "ACB"); //false
+
+//Operador logico 'and'
