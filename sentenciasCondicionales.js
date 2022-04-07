023e143b27dd3db8f412f88596a7c2233cfdb7d6
jorgecruzprof@gmail.com <jorgecruzprof@gmail.com>
2022-04-07 00:08:56 -0500

    7 de abril, 12:06 am

--- sentenciasCondicionales.js
+++ sentenciasCondicionales.js
@@ -7,8 +7,27 @@ if (condición) {
 */
 
 if (true) {
-  console.log("funciona")
+  console.log("la condición es verdadero")
+} else {
+  console.log("la condición es falsa")
 }
 
 //cláusula "else"
 
+ // Se puede agrgar una clausula "else" para casos falsos
+ 
+ if (1) {
+   console.log("la condición es 1")
+ } else if (2) {
+   console.log("la condición es 2")
+ } else {
+   console.log("la condición es diferente de 1 o 2")
+ }
+ 
+ //Cláusula "else if"
+ 
+ //En caso de que existan más estados que verificar, se pueden agregar clausulas "else if" para verificar cada estado adicional
+ 
+ //Condicionales lógicas
+ 
+ //es importante determinar la lógica de las condicionales para que está se apliquen de forma correcta
